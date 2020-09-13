04d9eb3e93b85cc85571b226114c414bc8381651
NAS <56131958+NAVSmith@users.noreply.github.com>
2020-09-13 22:53:23 +0200

    build the seleton for the data splitting class

--- model/split_data.py
+++ model/split_data.py
@@ -8,9 +8,10 @@
 # imports
 import sys
 
-import pandas as pd
+
 import numpy as np
-from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
 
 class Split_data:
     """
@@ -22,23 +23,92 @@ class Split_data:
 
     # init function
 
-    def __init__(self):
+    def __init__(self, project_dir_name):
         """
 
         :param data_path:
         """
+        # path
+        self.path = self.directory_path_processed + project_dir_name + '/'
+
+        # infomration about that data
+        self.train_is_data_balanced = False
+
+        # validate data
+        self.x_val = None
+        self.y_val = None
+
+        # train data
+        self.x_train = None
+        self.y_train = None
+
+
+        # loading the data
+        raw_features, raw_label = self.load_data()
+
+        # splitting the data
+        raw_train_features, raw_train_label = self.spliting_the_data(raw_features, raw_label)
+        # dumping the big files to make room in the ram
+        del raw_features, raw_label
+
+        # checking if data is balanced
+        # not working at the time fix it in the future
+
+        if not self.train_is_data_balanced:
+            self.balance_train_data()
+
+
+
+        #self.check_if_data_is_balanced(raw_train_label)
+
+
+    def load_data(self):
+
         print('load the data to be train')
-        file_name = input('please enter name of the csv file: ')
-        path = self.directory_path_processed + file_name + '.csv'
+        # file_name = input('please enter name of the data file: ')
+        print(self.path)
         try:
-            data = np.genfromtxt(path, delimiter=',')
-            print(data[0])
+            raw_label = np.genfromtxt(self.path + 'label.csv', delimiter=',')
+            print(raw_label[0])
+            raw_features = np.genfromtxt(self.path + 'features.csv', delimiter=',')
+            print(raw_features[0])
         except:
+            # make this more detailed
             print('something went wrong please enter a valid scv file form the processed_files directory')
+        return raw_features, raw_label
 
 
 
+    def spliting_the_data(self, features_data, label_data):
+        """
+        based splitting funtion build on it
+        :return:
+        """
+        # using sklearn to split
+        # seving the validate data into the class as it will no change
+        print('spliting data')
+        self.x_train, self.x_val, self.y_train, self.y_val = train_test_split(features_data,
+                                                          label_data,
+                                                          test_size=.2,
+                                                          random_state=12)
+        return x_train, y_train
 
 
+    def check_if_data_is_balanced(self, labels_data):
+        """
+
+        :param labels_data:
+        :return:
+        """
+        #fix it if there time
+        unique_val = list(np.unique(labels_data, return_counts=True))
+        label_zero = zip(unique_val[0][0], unique_val[1][0])
+        label_one = zip(unique_val[0][1], unique_val[1][1])
+        print(label_zero, label_one)
 
 
+    def balance_train_data(self):
+        # resampling the traninging data to change the balnce the data
+        sm = SMOTE(random_state=12)
+        self.x_train, self.x_train = sm.fit_sample(self.x_train, self.y_train)
+        self.train_is_data_balanced = True
--- model/test_file.py
+++ model/test_file.py
@@ -1,4 +1,4 @@
 from data_preprocessing import Base_data_preprocessing
 from split_data import Split_data
-df = Base_data_preprocessing('./../data/eemployes_data.csv')
-#df = Split_data()
+#df = Base_data_preprocessing('./../data/eemployes_data.csv')
+df = Split_data('initial_data_for_model')
